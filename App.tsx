1d251ebf577bd1e953a1cc33e96afc73605c8ddf
Murilo Lympius <murilol12345@gmail.com>
2025-04-04 20:20:08 -0300

    Implementação das Funcionalidades de Agendamento

--- App.tsx
+++ App.tsx
@@ -1,27 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import AppRoutes from './src/routes';
+import theme from './src/styles/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Engenharia da Computação</Text>
-
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={theme.colors.primary}
+        />
+        <AppRoutes />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#670102',
-    justifyContent: 'center', // Centraliza verticalmente
-    alignItems: 'center',     // Centraliza horizontalmente
-  },
-
-  texto: {
-    fontSize: 25,
-    color: '#FFFFFF', // White color
-  },
-
-});
